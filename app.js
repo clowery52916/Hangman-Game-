35781acf0c54a6a92b4593e8f8a8014cc63d2816
clowery <clowery52916@gmail.com>
2018-02-05 18:04:51 -0500

    updating game

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('JS is working!');
+
+let userName = prompt ('Hello! What\'s your name?');
+
+   (userName !== null) 
+    alert ('Hey there, ' + userName + '. I am really excited to play with you!');
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+
