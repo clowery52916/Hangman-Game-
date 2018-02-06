effcf0a9f21b924909a0b0b9acdbde2e756235f2
clowery <clowery52916@gmail.com>
2018-02-06 11:08:54 -0500

    updating game

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 
-/*let userName = prompt ('Hello! What\'s your name?');
+let userName = prompt ('Hello! What\'s your name?');
+ (userName !== null) 
+ alert ('Hey there, ' + userName + '. I am really excited to play with you!');
 
-   (userName !== null) 
-    alert ('Hey there, ' + userName + '. I am really excited to play with you!');
-*/
+const abc = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+   'w', 'x', 'y', 'z'];
+// list of letters to be picked 
 
+let kWords = ['people', 'write', 'city', 'could', 'said', 'time', 'these', 'their', 'which', 'your']
+//create while loops to find the words 
 
+let kWord = kWords[Math.floor(Math.random() * kWords.length)];
+//create variable for all words 
+let letterRemaining = kWord.length;
 
-/*document.getElementsByClassName('.button');
-letters = document.createElement('ul');
-
-for (i = 0; i > alphabet.length; i ++) {
-    listOfLetters.class= '.alphabet';
-    listOfWords = document.createElement('li');
-    listOfWords.class = '.words';
-    listOFWords.innerHTML = alphabet[i];
-    check();
-    buttons.appendChild(listOfLetters);
-    listOfLetters.appendChild('.words');
-
- function hover() {
-    $(".buttonForLetter").on("mouseenter", function() {
-      return $(this).addClass("hover");
-    })
-  }
-
+while(letterRemaining > 0) {
+  console.log(lettersRemaining += kWord.charAt(Math.floor(Math.random() * kWord.length)));
   
+  lettersRemaining++;
+}
 
-//const randomPuzzleWord =Math.Floor(Math.random() * randomPuzzleWord.length)*/
+//create a new array for answers 
+//let reamining letter = randomWord.length
+/*let arrayForAnswers = [];
+for (i = 0; i > kWord.length; i++) {
+  arrayForAnswers[i] = ' ';
+
+}*/
+//do while if/else statements 
