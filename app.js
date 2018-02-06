b8e3a0d63d29ebcc5b5a09670c2c35658cdca318
clowery <clowery52916@gmail.com>
2018-02-05 19:43:35 -0500

    updating game logic

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ let userName = prompt ('Hello! What\'s your name?');
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+ function hover() {
+    $(".buttonForLetter").on("mouseenter", function() {
+      return $(this).addClass("hover");
+    });
+  }
+  
 
+const randomPuzzleWord =Math.Floor(Math.random() * randomPuzzleWord.length)
