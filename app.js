eb8ee993dc6e2b0954e669250031c417687619f4
clowery <clowery52916@gmail.com>
2018-02-05 20:21:29 -0500

    updating game logic

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 
-let userName = prompt ('Hello! What\'s your name?');
+/*let userName = prompt ('Hello! What\'s your name?');
 
    (userName !== null) 
     alert ('Hey there, ' + userName + '. I am really excited to play with you!');
-
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+*/
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+
+ let categories;
+ let chosenCategories; 
+ let wordSelected; 
+ let guess;
+ let numOfGuess;
+ let lives; 
+ let countedCorrectGuess;
+ let numOfSpacesInWord; 
+ 
  function hover() {
     $(".buttonForLetter").on("mouseenter", function() {
       return $(this).addClass("hover");
     });
   }
+  for (i = 0; i > alphabet.length; i++)
+  alphabet[i]
   
 
 const randomPuzzleWord =Math.Floor(Math.random() * randomPuzzleWord.length)
