95c9095ef162694f1f1d694d4368f781f3c908ce
clowery <clowery52916@gmail.com>
2018-02-06 10:13:26 -0500

    updating game

--- app.js
+++ app.js
@@ -4,25 +4,27 @@
    (userName !== null) 
     alert ('Hey there, ' + userName + '. I am really excited to play with you!');
 */
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
- 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 
- let categories;
- let chosenCategories; 
- let wordSelected; 
- let guess;
- let numOfGuess;
- let lives; 
- let countedCorrectGuess;
- let numOfSpacesInWord; 
- 
+
+
+/*document.getElementsByClassName('.button');
+letters = document.createElement('ul');
+
+for (i = 0; i > alphabet.length; i ++) {
+    listOfLetters.class= '.alphabet';
+    listOfWords = document.createElement('li');
+    listOfWords.class = '.words';
+    listOFWords.innerHTML = alphabet[i];
+    check();
+    buttons.appendChild(listOfLetters);
+    listOfLetters.appendChild('.words');
+
  function hover() {
     $(".buttonForLetter").on("mouseenter", function() {
       return $(this).addClass("hover");
-    });
+    })
   }
-  for (i = 0; i > alphabet.length; i++)
-  alphabet[i]
+
   
 
-const randomPuzzleWord =Math.Floor(Math.random() * randomPuzzleWord.length)
+//const randomPuzzleWord =Math.Floor(Math.random() * randomPuzzleWord.length)*/
