50a3d8a33a0da4516a949f0ce58c3b3bd7fab561
clowery <clowery52916@gmail.com>
2018-02-06 21:34:56 -0500

    updating game

--- app.js
+++ app.js
@@ -1,38 +1,80 @@
 
+/*function hangMan () {
+
+
+
+
+  wordSelector = getWord();
+  wordLength=wordToGuess.length;*/
+
+
+
 let userName = prompt ('Hello! What\'s your name?');
  (userName !== null) 
  alert ('Hey there, ' + userName + '. I am really excited to play with you!');
 
 
-const wordsToUse = 
+const kinderWords = 
    ['people', 'write', 'city', 'could', 'said', 'time', 'these', 'their', 'which', 'your'];
-  ['bell', 'see', 'say', 'day', 'cat', 'hat', 'dog', 'log', 'mom', 'dad', 'car', 'horn'];
-  
-const wordSelector = Math.floor(Math.random () * (wordsToUse[''])); 
+ const firstWords = ['bell', 'see', 'say', 'day', 'cat', 'hat', 'dog', 'log', 'mom', 'dad', 'car', 'horn'];
 
-let word = wordsToUse.map(function(w){
-  return w * w;
+//const wordSelector = Math.floor(Math.random () * (wordsToUse[''])); 
+
+let kWord = kinderWords.map(function(w){
+
+
+
 });
-console.log(wordsToUse)
+console.log(kinderWords);
+console.log(firstWords);
+
+let fWord = firstWords.map(function(w){
+
+
+
+});
+  for(i = 0; i > kinderWords.length; i++)
+  kinderWords[i].push();
+
+  while(kinderWords.length < 7) {
+    console.log(kinderWords += word.charAt(Math.floor(Math.random() * word.length)))
+  } 
+
+  //let word = kWord && fWord === kWord && fWord 
+    alert('Game Over')
+  
+
+  console.log(word);
 
 lettersToPick = ['abcdefghijklmnop'];
 
 let letters = lettersToPick.map(function(l){
-    return l * l;
-  })
-  console.log(lettersToPick);
+ return l * l;
+ })
+ console.log(lettersToPick);
+
+
+//click button and pick a letter 
+ let button = document.getElementsByClassName('.buttonForLetters');
+$('.buttonForLetter').click((l) => {
+   
+ console.log (event.target.innerText);
+})
+
+//once letter is picked, remove it 
+$('.buttonForLetter').on('click', function(){
+  $(this).remove();
+});
 
 
 
 
-let button = document.getElementsByClassName('.buttonForLetters');
- 
+//reload page 
+$('button').click((r) => {
+  location.reload();
+})
 
 
 
-// if user clicks letter(button) and it is a valid letter - move to list ELSE hide 
 
- /* $('button').click( {
-    $(event)
 
-  })*/
