5c6bc271c133d30c49d33d0b12e0ce4cb68296e2
clowery <clowery52916@gmail.com>
2018-02-07 15:29:36 -0500

    update

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-
-/*function hangMan () {
-
-
-
-
-  wordSelector = getWord();
-  wordLength=wordToGuess.length;*/
-
-
-
 let userName = prompt ('Hello! What\'s your name?');
- (userName !== null) 
- alert ('Hey there, ' + userName + '. I am really excited to play with you!');
-
+ if (userName !== null) {
+   alert ('Hey there ' + userName +"" + '! I am really excited to play with you!');
+ } 
 
 const firstWords = 
    ['people', 'write', 'city', 'could', 'said', 'time', 'these', 'their', 'which', 'your'];
@@ -30,33 +19,6 @@ let wordOnPage = Array.from(new Set(output));
 console.log(wordOnPage);
 
 
-
-//document.getElementById('kinderWords').textContent = wordSelector;
-//let kWord = kinderWords.map(function(w){
-
-
-
-//});
-//console.log(kinderWords);
-//console.log(firstWords);
-
-//let fWord = firstWords.map(function(w){
-//console.log(fWord);
-
-
-//let word = [];
-
-  //for(i = 0; i > kWord.length; i++) {
-   // kWord[i].push(word);
-  //}
-    //console.log(word);
-
-  //while(kWord.length < 7) {
-    //console.log(kWord += word.charAt(Math.floor(Math.random() * word.length)))
-  //} 
-
-
-
 lettersToPick = ['abcdefghijklmnop'];
 let randomLetter = Math.floor(Math.random) * (lettersToPick) -1; 
 
@@ -64,36 +26,8 @@ let randomLetter = Math.floor(Math.random) * (lettersToPick) -1;
 
  let letterToCompare = lettersToPick[randomLetter];
 
- console.log(letterToCompare);
+ console.log(letterToCompare)
 
- 
-
- /*function intersect_arrays(wordOnPage, lettersToPick) {
-  var sorted_wordOnPage = wordOnPage.concat().sort();
-  var sorted_lettersToPick = lettersToPick.concat().sort();
-  var common = [];
-  var wordOnPage_i = 0;
-  var lettersToPick_i = 0;
-
-  while (wordOnPage_i < wordOnPage.length
-         && lettersToPick_i < lettersToPick.length)
-  {
-      if (sorted_wordOnPage[wordOnPage_i] === sorted_lettersToPick[lettersToPick_i]) {
-          common.push(sorted_wordOnPage[wordOnPage_i]);
-          wordOnPage_i++;
-          lettersToPick_i++;
-      }
-      else if(sorted_wordOnPage[wordOnPage_i] < sorted_lettersToPick[lettersToPick_i]) {
-        wordOnPage_i++;
-      }
-      else {
-        lettersToPick_i++;
-      }
-  }
-  return common;
-}
-console.log(intersect_arrays(wordOnPage, lettersToPick));*/
-//click button and pick a letter 
  let button = document.getElementsByClassName('.buttonForLetters');
 $('.buttonForLetter').click((l) => {
    
@@ -105,15 +39,9 @@ $('.buttonForLetter').on('click', function(){
   $(this).remove();
 });
 
-
-
-
 //reload page 
 $('button').click((r) => {
   location.reload();
 })
-
-
-
 
 
