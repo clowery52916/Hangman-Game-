32ec59074a9981c365e2f8eab7d38fa15f8603ef
clowery <clowery52916@gmail.com>
2018-02-07 19:43:53 -0500

    updating underscores

--- app.js
+++ app.js
@@ -2,37 +2,45 @@ let userName = prompt ('Hello! What\'s your name?');
  if (userName !== null) {
    alert ('Hey there ' + userName +"" + '! I am really excited to play with you!');
  } 
+ //words for game 
  const kinderWords = ['bell', 'see', 'say', 'day', 'cat', 'hat', 'dog', 'log', 'mom', 'dad', 'car', 'horn'];
 
-//]
+// rando words 
 let getRandomWord = function(str) {
   return kinderWords[Math.floor(Math.random() * kinderWords.length)];
 }
 let output = [].map.call(kinderWords, getRandomWord );
 
-
 let wordOnPage = Array.from(new Set(output));
-
 
 wordOnPage.random = function(){
   return this [Math.floor(Math.random()*this.length)];
 }
-let wordsShown = wordOnPage.random();
+let wordShown = wordOnPage.random();
+(wordShown);
+console.log(wordShown);
+console.log(wordShown.length);
+//words:'bell', 'see', 'say', 'day', 'cat', 
+//'hat', 'dog', 'log', 'mom', 'dad', 'car', 'horn'
 
 //underscore to insert words 
-let underscore = "";
-  for (i = 0; i > wordsShown.length; i++){
-    underscores = underscores + '_';
+let underscores = ' ';
+  for (let i = 0; i < wordShown.length; i++){
+    underscores = underscores + "_";
   }
+  $(document).ready(function(){
+$('#wordContainer').append(`<span></span>`)
 
-
+});
+console.log(underscores)
+//buttons
  let button = document.getElementsByClassName('.buttonForLetters');
 $('.buttonForLetter').click((l) => {
    
  console.log (event.target.innerText);
 })
 
-//once letter is picked, remove it 
+//once user picks letter remove it, remove it 
 $('.buttonForLetter').on('click', function(){
   $(this).remove();
 });
@@ -45,8 +53,9 @@ $('button').click((r) => {
 //let diff = wordOnPage;
 $('.buttonForLetter').each((number, value) => {
   let diff = $(this).attr('span');
+
  /* function myFunction( ){
-  let w = wordToGet.match(/ll/);
+
 document.getElementById('#wordContainer').innerHTML = w;
   //if (wordOnPage.indexOf(x) === 0){
    console.log(diff);
